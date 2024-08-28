94baee81f33fcf3a2ef8bd0fd8caf49e3f15b731
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-08-27 18:43:12 -0600

    Modificación para que se mantenga el valor en el text-area de descripcion y al momento de estar en la vista  de editar te regrese a la vista del producto

--- resources/views/product/edit-producto.blade.php
+++ resources/views/product/edit-producto.blade.php
@@ -7,7 +7,7 @@
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Modelorama</span>
         </a>
         <div>
-          <a href="{{ route('producto.index') }}" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><< Regresar</a>
+          <a href="{{ route('producto.show', $productDetail->id)}}" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><< Regresar</a>
         </div>
       </div>
       </nav>
@@ -44,7 +44,7 @@
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción</label>
                 <textarea id="desc" name="desc" rows="4"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Descripción del producto..."></textarea>
+                    placeholder="Descripción del producto...">{{old('desc', $productDetail->desc)}}</textarea>
             </div>
             <div class="mb-5">
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Guardar</button>
